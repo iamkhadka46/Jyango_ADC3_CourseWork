4dc8387fc557e25898a91f4ebc9f3cb4e012fb05
iamkhadka46 <58580576+iamkhadka46@users.noreply.github.com>
2020-02-09 23:34:48 +0545

    CRUD operation in REST API and bug fixes.

--- CAS/restapi/urls.py
+++ CAS/restapi/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('courses/<int:pk>/', views.update_api_data, name = "update_api_data"),
     path('api/assignments/<int:PAGENO>/<int:SIZE>', views.api_assignments, name="api_assignments_data"), 
     path('api/posts/<int:PAGENO>/<int:SIZE>', views.api_posts, name="api_posts_data"),
+    path('courses/create/', views.create_course, name = "create_course"),
 ]
--- CAS/restapi/views.py
+++ CAS/restapi/views.py
@@ -1,6 +1,6 @@
 
-
-from main.models import Course
+from django.shortcuts import render, redirect
+from main.models import *
 from django.http import HttpResponse,JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 import json
@@ -19,21 +19,40 @@ def update_api_data(request, pk):
     if request.method == "GET":
         return JsonResponse({"course_title": course.course_title, "course_semester": course.course_semester, "date":course.date})
 
-    else:
-        json_data = request.body.decode('utf-8')
+    elif request.method == "PUT":
+        json_data = request.body.decode("utf-8")
         update_data = json.loads(json_data)
-        course.course_title = update_data['course_title']
-        course.course_semester = update_data['course_semester']
-        course.date = update_data['date']
+        course.course_title = update_data["course_title"]
+        course.course_semester = update_data["course_semester"]
+        course.date = update_data["date"]
         course.save()
 
-        return JsonResponse({'message': 'Succesfully Completed'})
+        return JsonResponse({"message": "Succesfully Completed"})
+    
+    elif request.method == "DELETE":
+        course.delete()
+        return JsonResponse({"Success":"Course Successfully Deleted!!"})
+
+@csrf_exempt
+def create_course(request):
+    if request.method == "POST":
+        json_data = request.body.decode('utf-8')
+        create_course = json.loads(json_data)
+        course_title = create_course['course_title']
+        course_semester = create_course['course_semester']
+        date = create_course['date']
+        course = Course.objects.create(course_title=course_title, course_semester = course_semester, date = date)
+        try:
+            course.save()
+            return JsonResponse({"Success":"Course has been added successfully!"})
+        except:
+            return JsonResponse({"Error":"Course could not be added!"})
 
 def api_assignments(request,PAGENO,SIZE): # pagination for assignment
     if request.method == "GET":
         skip = SIZE * (PAGENO -1)
         assignments = Assignment.objects.all() [skip:(PAGENO * SIZE)]
-        dict_type = {"assignments": list(assignments.values("assign_file", "course", "course_id", "date", "due_date", "id", "teacher", "teacher_id"))}
+        dict_type = {"assignments": list(assignments.values("assign_file", "course", "date", "due_date", "teacher"))}
     return JsonResponse(dict_type)
 
 def api_posts(request,PAGENO,SIZE): # pagination for post
