f6e522fbdf3372c5c08dc7e0e9dc38cc386ee806
iamkhadka46 <58580576+iamkhadka46@users.noreply.github.com>
2020-02-09 13:16:00 +0545

    some updates

--- CAS/restapi/views.py
+++ CAS/restapi/views.py
@@ -7,9 +7,8 @@ from main.models import Course
 
 @csrf_exempt
 def api_data(request):
-    
+    course = Course.objects.all() #calling course object
     if request.method == "GET":
-        course = Course.objects.all() #calling course object
         dict_type = {"courses": list(course.values("course_title", "course_semester"))} #converting to json
         return JsonResponse(dict_type)
 
@@ -17,12 +16,12 @@ def api_data(request):
 def update_api_data(request, pk):
     course = Course.objects.get(pk = pk)
     if request.method == "GET":
-        return JsonResponse({"course": course.course_title, "semester": course.course_semester})
+        return JsonResponse({"course_title": course.course_title, "semester_title": course.course_semester})
 
     else:
         json_data = request.body.decode('utf-8')
         update_data = json.loads(json_data)
-        course.course_title = update_data['title']
-        course.course_semester = update_data['semester']
+        course.course_title = update_data['course_title']
+        course.course_semester = update_data['course_semester']
         course.save()
         return JsonResponse({'message': 'Succesfully Completed'})
