54036e3109901651f6e61bef7d2afa44e871de16
iamkhadka46 <58580576+iamkhadka46@users.noreply.github.com>
2020-02-10 11:08:12 +0545

    Some updates.

--- CAS/main/models.py
+++ CAS/main/models.py
@@ -60,7 +60,8 @@ class Submission(models.Model):
     student = models.ForeignKey(Student, on_delete = models.CASCADE)
     teacher = models.ForeignKey(Teacher, on_delete = models.CASCADE, null = True)
 
-    
+    def __str__(self):
+        return str(self.sub_file)
 
 class UploadFile(models.Model):
     title = models.CharField(max_length=100)
