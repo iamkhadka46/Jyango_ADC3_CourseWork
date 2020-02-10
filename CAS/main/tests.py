39b58168dce41e4f958c69a87a65ba2a1226323a
iamkhadka46 <58580576+iamkhadka46@users.noreply.github.com>
2020-02-10 13:20:48 +0545

    Test Cases added.

--- CAS/main/tests.py
+++ CAS/main/tests.py
@@ -1,12 +1,12 @@
 from django.test import TestCase
 from django.urls import reverse
 from main.models import Course, Teacher
+from django.utils import timezone
 from datetime import datetime
-from django.contrib.auth.models import User, AbstractUser
 
 class HeraldTest(TestCase):
     def setUp(self):
-        Course.objects.create(course_title = 'Maths', course_semester = "Spring", date = datetime.now())
+        Course.objects.create(course_title = 'Maths', course_semester = "Spring", date = datetime.now(tz=timezone.utc))
 
     def test_ORM(self):
         c1 = Course.objects.get(course_title = 'Maths')
@@ -14,8 +14,36 @@ class HeraldTest(TestCase):
 
 class RandomTest(TestCase):
     def setUp(self):
-        Course.objects.create(course_title = 'Maths', course_semester = "Spring", date = datetime.now())
+        Course.objects.create(course_title = 'Maths', course_semester = "Spring", date = datetime.now(tz=timezone.utc))
 
     def test_ORM(self):
         c1 = Course.objects.get(course_semester = 'Spring')
-        self.assertEqual(c1.course_semester, 'Autumn')
+        self.assertNotEqual(c1.course_semester, 'Autumn')
+
+class Test3(TestCase):
+    def setUp(self):
+        Course.objects.create(course_title = 'Physics', course_semester = "Fall", date = datetime.now(tz=timezone.utc))
+        Course.objects.create(course_title = 'Maths', course_semester = "Spring", date = datetime.now(tz=timezone.utc))
+
+    def test_ORM(self):
+        c3 = Course.objects.get(course_semester = 'Fall')
+        c4 = Course.objects.get(course_semester = 'Spring')
+        self.assertIs(c3, c4)
+
+class Test4(TestCase):
+    def setUp(self):
+        Course.objects.create(course_title = 'OOPD', course_semester = "Fall", date = datetime.now(tz=timezone.utc))
+        Course.objects.create(course_title = 'Database', course_semester = "Spring", date = datetime.now(tz=timezone.utc))
+
+    def test_ORM(self):
+        c3 = Course.objects.get(course_title = 'OOPD')
+        c4 = Course.objects.get(course_semester = 'Spring')
+        self.assertIsNot(c3, c4)
+
+class Test5(TestCase):
+    def setUp(self):
+        Course.objects.create(course_title = 'Physics', course_semester = "Fall", date = datetime.now(tz=timezone.utc))
+
+    def test_ORM(self):
+        c6 = Course.objects.get(course_semester = 'Fall')
+        self.assertIsNone(c6)
